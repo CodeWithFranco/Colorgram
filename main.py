425d9d3e3b11a4fc692a258d652131c4cc6ff61d
CodeWithFranco <120446758+CodeWithFranco@users.noreply.github.com>
2023-12-12 21:57:21 -0800

    Initial

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import colorgram
 
 # Extract 7 colors from image
-colors = colorgram.extract('Colorgram.jpeg', 7)
-print(colors)
+colors = colorgram.extract('Colorgram.jpeg', 30)
+
+# New list
+rgb_colors = []
+
+# Get the color code
+for color in colors:
+    rgb_colors.append(color.rgb)
+
+print(rgb_colors)
